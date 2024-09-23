e130da1d6f62f7658c52f2bdb27f53565f9582ab
hylo <hylo@posteo.de>
2024-09-23 18:13:29 +0200

    chore: add gitignore

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	// import Header from './Header.svelte';
+	import '../app.css';
+</script>
+
+<header class="h-8 bg-slate-500"></header>
+<main class="h-full">
+	<slot />
+</main>
+<footer class="h-8 bg-slate-500"></footer>
+
+<style>
+</style>
