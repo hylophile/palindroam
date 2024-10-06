a0c4e2231d5ff5736feb6da36369bbc9ea7ac2cc
hylo <hylo@posteo.de>
2024-10-06 17:22:07 +0200

    fix: types

--- app/src/routes/embed/[noteID]/+server.ts
+++ app/src/routes/embed/[noteID]/+server.ts
@@ -1,12 +1,14 @@
-export async function GET({ params }) {
-	// const number = Math.floor(Math.random() * 6) + 1;
+import type { RouteParams, } from './$types'
 
-	const noteID = params.noteID.endsWith('.html') ? params.noteID : `${params.noteID}.html`;
-	const page = await import(`/src/lib/notes/${noteID}?raw`);
+export async function GET({ params }: { params: RouteParams }) {
+  const noteID = params.noteID.endsWith('.html') ? params.noteID : `${params.noteID}.html`;
+  const page = await import(`/src/lib/notes/${noteID}?raw`);
 
-	return new Response(page.default, {
-		headers: {
-			'Content-Type': 'text/html'
-		}
-	});
+  return new Response(page.default, {
+    headers: {
+
+      'Content-Type': 'text/html',
+      // "cache-control": "max-age=60",
+    }
+  });
 }
