237172705c48b21fff2040fc7fde62b8358099b2
Nathanael Sandy <nathanael.sandy@fokus.fraunhofer.de>
2024-10-12 21:08:01 +0200

    chore: fix type errors

--- gen_backlinks.py
+++ gen_backlinks.py
@@ -1,18 +1,32 @@
+from collections.abc import Iterable
 import json
 from pathlib import Path
 from bs4 import BeautifulSoup
 
-def extract_links_from_html(file_path):
-    """Extracts all href links from the given HTML file."""
-    with file_path.open('r', encoding='utf-8') as file:
-        soup = BeautifulSoup(file, 'html.parser')
-        links = {a['href'].lstrip('./') for a in soup.find_all('a', href=True) if not a['href'].startswith('https://')}
+
+def extract_links_from_html(file_path: Path) -> set[str]:
+    """
+    Extracts all href links from the given HTML file.
+
+    :param file_path: The path.
+    :returns: The set of link hrefs.
+    """
+    with file_path.open("r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file, "html.parser")
+        link_results: Iterable[dict[str, str]] = soup.find_all("a", href=True)
+
+        links = {
+            a["href"].lstrip("./")
+            for a in link_results
+            if not a["href"].startswith("https://")
+        }
     return links
 
-def build_backlinks(directory):
+
+def build_backlinks(directory: Path) -> dict[str, list[str]]:
     """Builds a dictionary with links as keys and their backlinks (HTML files) as values."""
-    backlinks = {}
-    html_files = directory.rglob('*.html')
+    backlinks: dict[str, list[str]] = {}
+    html_files = directory.rglob("*.html")
 
     for html_file in html_files:
         links = extract_links_from_html(html_file)
@@ -25,12 +39,13 @@ def build_backlinks(directory):
 
     return backlinks
 
+
 if __name__ == "__main__":
-    base_dir = Path('./app/src/lib/notes')
-    output_file = Path('app/src/lib/backlinks.json')
+    base_dir = Path("./app/src/lib/notes")
+    output_file = Path("app/src/lib/backlinks.json")
     backlinks = build_backlinks(base_dir)
 
-    with output_file.open('w', encoding='utf-8') as f:
+    with output_file.open("w", encoding="utf-8") as f:
         json.dump(backlinks, f, indent=4)
 
     print(f"Backlinks saved to {output_file}")
